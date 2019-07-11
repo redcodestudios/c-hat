f433405c8d5b008eaec625005a8a3159184630ef
rodrigocam <rodrigo.redcode@gmail.com>
2019-07-11 15:39:45 -0300

    trash

---
+++ premake4.lua
@@ -0,0 +1,23 @@
+#!lua
+
+solution "c-hat"
+    configurations { "linux", "debug", "release"}
+
+project "c-hat"
+    kind "ConsoleApp"
+    language "C"
+    targetdir "bin"
+
+    files { "src/inc/**.h,", "src/**.c" }
+
+    configuration "debug"
+        defines { "DEBUG" }
+        flags { "Symbols" }
+
+    configuration "release"
+        defines { "NODEBUG" }
+        flags { "Optimize" }
+    
+    configuration "linux"
+        buildoptions {"-pthread", "-lGL", "-lm", "-lrt", "-Wall", "-Wextra"}
+        linkoptions { "-pthread", "-lrt" }
